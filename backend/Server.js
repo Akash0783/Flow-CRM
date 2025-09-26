97f01d0bf8575609f5f5de8fa9829e65f304fe9b
Akash0783 <akashkumar41675@gmail.com>
2025-09-26 08:04:15 +0530

    Added Frontend URL in Backend

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    "https://flow-crm-1.onrender.com"  // replace with actual frontend URL
+    "https://flow-crm-frontend.onrender.com"  // replace with actual frontend URL
   ],
   credentials: true
 }));
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
-  baseURL: API_BASE_URL, // e.g. https://flow-crm-5dq6.onrender.com/api
+  baseURL: API_BASE_URL, 
   headers: { "Content-Type": "application/json" },
 });
 
