fd0cb908c06f7590fb60408f45a0bbbac3dbdc9d
Akash0783 <akashkumar41675@gmail.com>
2025-10-05 09:36:34 +0530

    URL CHANGED

--- backend/Server.js
+++ backend/Server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "https://68e1e0bd7a1a6429f789520e--aktrack.netlify.app", 
+    origin: "https://aktrack.netlify.app", 
     credentials: true,
 }));
 
